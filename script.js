62cfdda5ec926b6a4ef12e996d3bfe2c0cd8472a
Daniel <dselasea@gmail.com>
2022-11-14 04:39:12 +0100

    Modified stylesheet and script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Testing")
+let num1 = ""
+let num2 = ""
+let operator = ""
+let calculated = ""
+
+let screen = document.querySelector("#screen")
+let numbers = document.querySelectorAll(".numbers");
+let operatotions = document.querySelectorAll(".operator");
+let clear = document.querySelector("#clear")
+let back = document.querySelector("back")
+let equal = document.querySelector(".equal")
+let percent = document.querySelector(".percentage")
+
+numbers.forEach(number => {
+  number.addEventListener("click", (e) => {
+    console.log(e.target.textContent)
+  })
+})
