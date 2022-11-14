7beaca52b67041f5b27296f5691dd464cb5e6078
Daniel <dselasea@gmail.com>
2022-11-14 04:48:02 +0100

    Added button functionality for numbers and operators

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let num1 = ""
 let num2 = ""
 let operator = ""
 let calculated = ""
+let num = ""
 
 let screen = document.querySelector("#screen")
 let numbers = document.querySelectorAll(".numbers");
-let operatotions = document.querySelectorAll(".operator");
+let operations = document.querySelectorAll(".operator");
 let clear = document.querySelector("#clear")
 let back = document.querySelector("back")
 let equal = document.querySelector(".equal")
@@ -13,6 +14,22 @@ let percent = document.querySelector(".percentage")
 
 numbers.forEach(number => {
   number.addEventListener("click", (e) => {
+    if(num.length <= 10){
+      num += e.target.textContent
+      screen.textContent = num
+    }
+  })
+})
+
+operations.forEach(operation => {
+  operation.addEventListener("click", (e) => {
+    operator = operation
+    screen.textContent = e.target.textContent
     console.log(e.target.textContent)
   })
+})
+
+clear.addEventListener("click", () => {
+  screen.textContent = "0"
+  num = ""
 })
