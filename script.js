34733d792e5891c19340e3d3037ef16017c303d8
Daniel <dselasea@gmail.com>
2022-11-19 03:27:28 +0100

    Added javascript functionality

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
-let num1 = ""
-let num2 = ""
+let previousValue = ""
+let currentValue = ""
 let operator = ""
-let calculated = ""
-let num = ""
 
-let screen = document.querySelector("#screen")
+document.addEventListener("DOMContentLoaded", function(){
+  let screen = document.querySelector("#screen")
 let numbers = document.querySelectorAll(".numbers");
 let operations = document.querySelectorAll(".operator");
 let clear = document.querySelector("#clear")
-let back = document.querySelector("back")
+let back = document.querySelector("#back")
 let equal = document.querySelector(".equal")
 let percent = document.querySelector(".percentage")
 
 numbers.forEach(number => {
   number.addEventListener("click", (e) => {
-    if(num.length <= 10){
-      num += e.target.textContent
-      screen.textContent = num
+    if(currentValue.length <= 10){
+      currentValue += e.target.textContent
+      screen.textContent = currentValue
     }
   })
 })
 
 operations.forEach(operation => {
   operation.addEventListener("click", (e) => {
-    operator = operation
+    operator = e.target.textContent
+    previousValue = currentValue
+    currentValue = currentValue
     screen.textContent = e.target.textContent
-    console.log(e.target.textContent)
   })
 })
 
 clear.addEventListener("click", () => {
-  screen.textContent = "0"
-  num = ""
+  previousValue = ""
+  currentValue = ""
+  operator = ""
+  
+})
+
+back.addEventListener("click", () => {
+  previousValue = ""
+  currentValue = ""
+  operator = ""
+})
+
+equal.addEventListener("click", () => {
+  previousValue = Number(previousValue)
+  currentValue = Number(currentValue)
+  if(operator === "+"){
+    previousValue += currentValue
+  }else if(operator === "-"){
+    previousValue -= currentValue
+  }else if(operator === "*"){
+    previousValue *= currentValue
+  }else {
+    previousValue /= currentValue
+  }
+  previousValue = Math.round(previousValue * 1000) / 1000
+  previousValue = previousValue.toString()
+  currentValue = currentValue.toString()
+  console.log(currentValue, previousValue)
+})
 })
